d74509ac4be0a9d8a9fd0ee11d5f1536b795e28d
Jochen Schalanda <jochen@graylog.com>
2017-02-14 17:36:50 +0100

    Fix FindBugs warnings with level "HIGH"

--- graylog2-server/src/main/java/org/graylog2/bootstrap/commands/CliCommandHelp.java
+++ graylog2-server/src/main/java/org/graylog2/bootstrap/commands/CliCommandHelp.java
@@ -19,5 +19,5 @@ package org.graylog2.bootstrap.commands;
 import org.graylog2.bootstrap.CliCommand;
 
 /* shallow subclass to make it implement CliCommand */
-public class Help extends io.airlift.airline.Help implements CliCommand {
+public class CliCommandHelp extends io.airlift.airline.Help implements CliCommand {
 }
